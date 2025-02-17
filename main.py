3725ad330c955095749b19e207cffc49b264eb7f
Oxtiz <oxtizdan_job@gmail.com>
2025-02-18 00:04:27 +0300

    create spacy NER model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ctypes import alignment
+import os
 import string
 import pandas as pd
 import re
@@ -7,7 +8,7 @@ import spacy
 from spacy.tokens import DocBin
 
 
-def load_csv(_path: str) -> pd.DataFrame:
+def load_csv(_path) -> pd.DataFrame:
     """
     Функция открытия "csv" файла с определенными названиями столбцов
 
@@ -55,27 +56,23 @@ def _generate_training_data(_reg : str, _text='', _training_data=[], essence="PR
     if _reg == None:
         return _training_data
     
-    _total = list(re.finditer(_reg, _text))
+    _total = list(re.finditer(_reg, _text, re.I))
     _data = []
         
     for _essence in _total:
-    
         _data.append((_essence.start(), _essence.end(), essence))
             
-        _doc = (_text, _data)
+    _doc = (_text, _data)
 
-        _training_data.append(_doc)
+    _training_data.append(_doc)
 
     return _training_data
 
 
 def find_essences(_df: pd.DataFrame):
-    _df_lower = _df.copy()
-    _df_lower['essence'] = _df_lower['essence'].str.lower()
-    _df_lower['text'] = _df_lower['text'].str.lower()
 
     # Проход по всему датафрейму
-    for id, row in _df_lower.iterrows():
+    for id, row in _df.iterrows():
 
         row['text'] = row['text'].replace('&#32;', ' ')
         row['text'] = row['text'].replace('  ', ' ')
@@ -83,12 +80,8 @@ def find_essences(_df: pd.DataFrame):
         _first_word = f"\\b.*?{row['essence']}(?:.*? -|.*? —)"
         _first_word_text = re.match(_first_word, row['text'])
         
-        
         # Регулярные выражения для переборов сущностей
-        reg_with_dash = f"\\b{row['essence']}\\b —|\\b{row['essence']}\\b -"
-        reg_dash = f'(?:{row['essence']}\\b.+? —|{row['essence']}\\b.+? -)'
-        reg_mention = f"\\b{row['essence']}(?:\\b|.*?\\b)"
-        reg_primary = f"\\b{row['essence']}(?:\\b|.*?\\b)"
+        reg_essence = f"\\b{row['essence']}[.]?\\w+?\\b"
         reg_prefix = f"\\b\\w+?{row['essence']}.*?\\b" # Слова с приставкой
         reg_prefix_with_dash = f"\\b\\w+?{row['essence']}.*?\\b -|\\b\\w+?{row['essence']}.*?\\b —"
 
@@ -97,11 +90,9 @@ def find_essences(_df: pd.DataFrame):
         sub_text = row['text'].split(',')
 
         # Поиск регулярного выражения
-        total_text_with_dash = re.search(reg_with_dash, row['text'])  # Ищет начало
-        total_text_dash = re.search(reg_dash, row['text'])  # Ищет сущность и - (большое тире)
-        total_text_braked = re.search(f"\\({row['essence']}\\)", sub_text[0])  # Ищет скобку в тексте
-        total_text_prefix = re.search(reg_prefix, row['text']) # Ищет слово с приставкой
-        total_text_prefix_with_dash = re.search(reg_prefix_with_dash, row['text']) # Ищет слово с приставкой
+        total_text_braked = re.search(f"\\({row['essence']}\\)", sub_text[0], re.I)  # Ищет скобку в тексте
+        total_text_prefix = re.search(reg_prefix, row['text'], re.I) # Ищет слово с приставкой
+        total_text_prefix_with_dash = re.search(reg_prefix_with_dash, row['text'], re.I) # Ищет слово с приставкой
 
         # Разбиение имени и фамилии
         total_text_fio = None
@@ -109,49 +100,53 @@ def find_essences(_df: pd.DataFrame):
         
         if len(total_split_fio) == 2:
             fio = f'{total_split_fio[0]} \\s\\w{2,}?\\s {total_split_fio[1]}'
-            total_text_fio = re.search(fio, row['text'])
+            total_text_fio = re.search(fio, row['text'], re.I)
 
-        if row['essence'] == 'циан':
-            print('lelel')
         if _first_word_text:
-            _generate_training_data(reg_primary, row['text'])
-        # # Первый проход - Ищет сущность и —/- (длинный/короткий дефис)
-        # if total_text_with_dash:
-        #     _generate_training_data(reg_primary, row['text'])
-
-
-        # # Третий проход с поиском зависимых слов и —/- (длинный/короткий дефис)
-        # elif total_text_dash:
-        #     _generate_training_data(reg_primary, row['text'])
-
+            _generate_training_data(reg_essence, row['text'])
 
         elif total_text_braked:
-            _generate_training_data(reg_primary, row['text'])
+            _generate_training_data(reg_essence, row['text'])
 
         elif total_text_fio:
             _generate_training_data(total_text_fio.group(), row['text'])
 
         # Вычисляем те строки, которые не находятся и вставляем сущность
-        elif row['essence'] not in row['text']:
+        elif row['essence'].lower() not in row['text'].lower():
             # Изменяем датасет и добавляем к основным сущностям
             _temp_text = f"{row['essence']} - {row['text']}"
+            if "+" in row['essence']:
+                row['essence'] = row['essence'].replace("+", "[\\+]")
             _generate_training_data(row['essence'], _temp_text)
 
         elif total_text_prefix_with_dash:
             _generate_training_data(reg_prefix, row['text'])
             
         elif total_text_prefix:
-            _temp_text = f"{row['essence']} - {row['text']}"
-            _generate_training_data(reg_primary, _temp_text)
+            _generate_training_data(reg_prefix, row['text'])
             
         else:
-            print(reg_mention)
-            _generate_training_data(reg_mention, row['text'], essence='MENTION')
+            _generate_training_data(reg_essence, row['text'], essence='MENTION')
     return _generate_training_data(None)
 
+def create_cfg(_data, _file_path):
+    nlp = spacy.blank("xx")
+
+    db = DocBin()
+    for text, annotations in _data:
+        doc = nlp(text)
+        ents = []
+        for start, end, label in annotations:
+            span = doc.char_span(start, end, label=label, alignment_mode='contract')
+            ents.append(span)
+        doc.ents = ents
+        db.add(doc)
+    db.to_disk(_file_path)
+    
 
 if __name__ == "__main__":
     train_path = getenv("TRAIN_PATH")
+    test_path = getenv("TEST_PATH")
 
     train_df = load_csv(train_path)  # Начальный (тренировочный) датафрейм
 
@@ -159,24 +154,13 @@ if __name__ == "__main__":
 
     training_data = find_essences(train_df)
 
-    nlp = spacy.blank("xx")
+    create_cfg(training_data, "./train.spacy")
+    
+    test_df = load_csv(test_path)  # Начальный (тренировочный) датафрейм
 
-    db = DocBin()
-    for text, annotations in training_data:
-        doc = nlp(text)
-        ents = []
-        if len(annotations) > 1:
-            lol =1
-        for start, end, label in annotations:
-            print(text[start:end])
-            span = doc.char_span(start, end, label=label, alignment_mode='contract')
-            ents.append(span)
-        doc.ents = ents
-        db.add(doc)
-    db.to_disk("./train.spacy")
+    test_df = drop_duplicate(test_df)  # Удаление дубликатов
 
-    # primary_df, mentions_df = filling_ids(train_df) # Основные сущности и дополнительные
-
-    # train_mentions = create_train_data(mentions_df) # Тренировочная выборка
-
-    # train_df.loc[train_df.duplicated(),:] # Проверка дубликатов
+    test_data = find_essences(test_df)
+    
+    create_cfg(test_data, "./dev.spacy")
+    
